d6bfe223343f9ce3412037566e2280dfd97cbc75
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2018-04-01 10:26:13 +0300

    Modified the draw line algorithm to support all the orientation and added stroke to drawed lines

--- Homework4/main.cpp
+++ Homework4/main.cpp
@@ -6,8 +6,9 @@
 
 #include "glut.h"
 
-#define LINES 10
-#define COLUMNS 10
+#define LINES 15
+#define COLUMNS 15
+#define MARGIN 0.1
 #define DIM 300
 #define PI 3.14
 
@@ -58,21 +59,21 @@ public:
 
 		double min = width > height ? height : width;
 
-		double xmax = (double)width / (double)min;
-		double ymax = (double)height / (double)min;
+		double xmax = (double)width / (double)min + MARGIN;
+		double ymax = (double)height / (double)min + MARGIN;
 
-
-		for (double i = 0; i < LINES + 1; i++) {
-			glBegin(GL_LINE_STRIP);
-			glVertex2f(-1.0 / xmax, (2.0 / (double)LINES * i - 1) / ymax);
-			glVertex2f(1.0 / xmax, (2.0 / (double)LINES * i - 1) / ymax);
-			glEnd();
-		}
 
 		for (double i = 0; i < COLUMNS + 1; i++) {
 			glBegin(GL_LINE_STRIP);
-			glVertex2f((2.0 / (double)COLUMNS * i - 1) / xmax, -1.0 / ymax);
-			glVertex2f((2.0 / (double)COLUMNS * i - 1) / xmax, 1.0 / ymax);
+			glVertex2f(-1.0 / xmax, (2.0 / (double)COLUMNS * i - 1) / ymax);
+			glVertex2f(1.0 / xmax, (2.0 / (double)COLUMNS * i - 1) / ymax);
+			glEnd();
+		}
+
+		for (double i = 0; i < LINES + 1; i++) {
+			glBegin(GL_LINE_STRIP);
+			glVertex2f((2.0 / (double)LINES * i - 1) / xmax, -1.0 / ymax);
+			glVertex2f((2.0 / (double)LINES * i - 1) / xmax, 1.0 / ymax);
 			glEnd();
 		}
 	}
@@ -83,8 +84,8 @@ public:
 
 		double min = width > height ? height : width;
 
-		double xmax = (double)width / (double)min;
-		double ymax = (double)height / (double)min;
+		double xmax = (double)width / (double)min + MARGIN;
+		double ymax = (double)height / (double)min + MARGIN;
 
 		float cx = (2.0 / (double)COLUMNS * line - 1) / xmax;
 		float cy = (2.0 / (double)LINES * column - 1) / ymax;
@@ -109,7 +110,7 @@ public:
 		glEnd();
 	}
 
-	void deseneazaLinie(int x0, int y0, int xn, int yn) {
+	void deseneazaLinie(int x0, int y0, int xn, int yn, int stoke) {
 		glColor3f(1.0, 0.1, 0.1); // rosu
 		glLineWidth(4.f);
 
@@ -124,7 +125,7 @@ public:
 		glEnd();
 		glLineWidth(1.f);
 
-		std::vector<Punct> list = deseneazaLinieAlgoritm(x0, y0, xn, yn);
+		std::vector<Punct> list = deseneazaLinieAlgoritm(x0, y0, xn, yn, stoke);
 
 		for (std::vector<Punct>::iterator it = list.begin(); it != list.end(); ++it) {
 			deseneazaPixel(it->getX(), it->getY());
@@ -132,25 +133,44 @@ public:
 
 	}
 
-	std::vector<Punct> deseneazaLinieAlgoritm(int x0, int y0, int xn, int yn) {
-		if (abs(yn - y0) < abs(xn - x0)) {
-			if (x0 > xn)
-				return deseneazaLinieX(xn, yn, x0, y0);
+	std::vector<Punct> deseneazaLinieAlgoritm(int x0, int y0, int xn, int yn, int stoke) {
+		double m = ((double)yn - (double)y0) / ((double)xn - (double)x0);
+		if (m > 0) {
+			if (abs(yn - y0) < abs(xn - x0)) {
+				if (x0 > xn)
+					return deseneazaLinieXPositive(xn, yn, x0, y0, stoke);
+				else
+					return deseneazaLinieXPositive(x0, y0, xn, yn, stoke);
+			}
 			else
-				return deseneazaLinieX(x0, y0, xn, yn);
+			{
+				if (y0 > yn)
+					return deseneazaLinieYPositive(xn, yn, x0, y0, stoke);
+				else
+					return deseneazaLinieYPositive(x0, y0, xn, yn, stoke);
+			}
 		}
-		else
-		{
-			if (y0 > yn)
-				return deseneazaLinieY(xn, yn, x0, y0);
+		else {
+			if (abs(yn - y0) < abs(xn - x0)) {
+				if (x0 > xn)
+					return deseneazaLinieXNegative(xn, yn, x0, y0, stoke);
+				else
+					return deseneazaLinieXNegative(x0, y0, xn, yn, stoke);
+			}
 			else
-				return deseneazaLinieY(x0, y0, xn, yn);
+			{
+				if (y0 > yn)
+					return deseneazaLinieYNegative(x0, y0, xn, yn, stoke);
+				else
+					return deseneazaLinieYNegative(xn, yn, x0, y0, stoke);
+			}
+			std::vector<Punct> puncte;
+			return puncte;
 		}
-				
-		return deseneazaLinieX(yn, y0, xn, x0);
+
 	}
 
-	std::vector<Punct> deseneazaLinieX(int x0, int y0, int xn, int yn)
+	std::vector<Punct> deseneazaLinieXPositive(int x0, int y0, int xn, int yn, int stoke)
 	{
 		std::vector<Punct> puncte;
 		int dy = yn - y0;
@@ -162,6 +182,10 @@ public:
 		int x = x0, y = y0;
 
 		puncte.push_back(Punct(x, y));
+		for (int i = 1; i <= stoke / 2; i++) {
+			puncte.push_back(Punct(x, y - i));
+			puncte.push_back(Punct(x, y + i));
+		}
 		while (x < xn)
 		{
 			if (d <= 0) {
@@ -176,12 +200,16 @@ public:
 				y++;
 			}
 			puncte.push_back(Punct(x, y));
+			for (int i = 1; i <= stoke / 2; i++) {
+				puncte.push_back(Punct(x, y - i));
+				puncte.push_back(Punct(x, y + i));
+			}
 		}
 
 		return puncte;
 	}
 
-	std::vector<Punct> deseneazaLinieY(int x0, int y0, int xn, int yn)
+	std::vector<Punct> deseneazaLinieYPositive(int x0, int y0, int xn, int yn, int stoke)
 	{
 		std::vector<Punct> puncte;
 
@@ -189,25 +217,115 @@ public:
 		int dx = xn - x0;
 
 		int d = dy - 2 * dx;
-		int dS = -2 * dx;
-		int dSE = 2 * (dy - dx);
+		int dE = -2 * dx;
+		int dNE = 2 * (dy - dx);
 		int x = x0, y = y0;
 
 		puncte.push_back(Punct(x, y));
+		for (int i = 1; i <= stoke / 2; i++) {
+			puncte.push_back(Punct(x - i, y));
+			puncte.push_back(Punct(x + i, y)); 
+		}
 		while (y < yn)
 		{
 			if (d > 0) {
 				/* alegem S */
-				d += dS;
+				d += dE;
 				y++;
+			}
+			else {
+				/* alegem SE */
+				d += dNE;
+				x++;
+				y++;
+			}
+
+			puncte.push_back(Punct(x, y));
+			for (int i = 1; i <= stoke / 2; i++) {
+				puncte.push_back(Punct(x - i, y));
+				puncte.push_back(Punct(x + i, y));
+
+			}
+		}
+		return puncte;
+	}
+
+	std::vector<Punct> deseneazaLinieXNegative(int x0, int y0, int xn, int yn, int stoke)
+	{
+		std::vector<Punct> puncte;
+		int dy = yn - y0;
+		int dx = xn - x0;
+
+		int d = 2 * dy + dx;
+		int dE = 2 * dy;
+		int dSE = 2 * (dy + dx);
+		int x = x0, y = y0;
+
+		puncte.push_back(Punct(x, y));
+		for (int i = 1; i <= stoke / 2; i++) {
+			puncte.push_back(Punct(x, y - i));
+			puncte.push_back(Punct(x, y + i));
+		}
+		while (x < xn)
+		{
+			if (d >= 0) {
+				/* alegem E */
+				d += dE;
+				x++;
 			}
 			else {
 				/* alegem SE */
 				d += dSE;
 				x++;
-				y++;
+				y--;
 			}
 			puncte.push_back(Punct(x, y));
+			for (int i = 1; i <= stoke / 2; i++) {
+				puncte.push_back(Punct(x, y - i));
+				puncte.push_back(Punct(x, y + i));
+			}
+		}
+
+		return puncte;
+	}
+
+	std::vector<Punct> deseneazaLinieYNegative(int x0, int y0, int xn, int yn, int stoke)
+	{
+		std::vector<Punct> puncte;
+
+		int dy = yn - y0;
+		int dx = xn - x0;
+
+		int d = dy + 2 * dx;
+		int dS = 2 * dx;
+		int dSE = 2 * (dy + dx);
+		int x = x0, y = y0;
+
+		puncte.push_back(Punct(x, y));
+		for (int i = 1; i <= stoke / 2; i++) {
+			puncte.push_back(Punct(x - i, y));
+			puncte.push_back(Punct(x + i, y));
+		}
+		while (y > yn)
+		{
+			if (d < 0) {
+				/* alegem S */
+				d += dS;
+				y--;
+			}
+			else {
+				/* alegem SE */
+				d += dSE;
+				x++;
+				y--;
+			}
+
+			puncte.push_back(Punct(x, y));
+			for (int i = 1; i <= stoke / 2; i++) {
+				puncte.push_back(Punct(x - i, y));
+				puncte.push_back(Punct(x + i, y));
+
+			}
 		}
 		return puncte;
 	}
@@ -219,8 +337,8 @@ private:
 	void getXY(int line, int column, float& outX, float& outY) {
 		double min = width > height ? height : width;
 
-		double xmax = (double)width / (double)min;
-		double ymax = (double)height / (double)min;
+		double xmax = (double)width / (double)min + MARGIN;
+		double ymax = (double)height / (double)min + MARGIN;
 
 		outX = (2.0 / (double)COLUMNS * line - 1) / xmax;
 		outY = (2.0 / (double)LINES * column - 1) / ymax;
@@ -246,9 +364,9 @@ void Display(void) {
 	//grilaCarteziana.deseneazaPixel(5, 5);
 	//grilaCarteziana.deseneazaPixel(5, 6);
 
-	grilaCarteziana.deseneazaLinie(0, 0, 6, 2);
+	grilaCarteziana.deseneazaLinie(0, 15, 15, 10, 3);
 
-	grilaCarteziana.deseneazaLinie(5, 2, 6, 8);
+	grilaCarteziana.deseneazaLinie(0, 0, 15, 7, 1);
 
 
 	glFlush();
